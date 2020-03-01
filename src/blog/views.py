d4ce8a546e36ac8bba4aaa3d51fd19ae7ed44d33
Franz Andrei M. Cruspero <franz.cruspero@gmail.com>
2020-03-01 23:44:22 +0800

    Finished djviews section

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.db.models import Q
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -36,7 +37,14 @@ def post_model_detail_view(request, slug):
     return render(request, template, context)
 
 def post_model_list_view(request):
+    search_query = request.GET.get("q", None)
     qs = PostModel.objects.all()
+    if search_query is not None:
+        qs = qs.filter(
+            Q(title__icontains=search_query) |
+            Q(slug__icontains=search_query) |
+            Q(content__icontains=search_query)
+        )
     template = "blog/list-view.html"
     context = {
         "object": qs,
@@ -85,3 +93,40 @@ def post_model_delete_view(request, slug):
 #         return HttpResponseRedirect("/login/")
 
 #   return render(request, template, context)
+
+def post_model_robust_view(request, slug=None):
+    obj = None
+    context = {}
+    success_message = "A new post was created!"
+    template = "blog/detail-view.html"
+
+    if slug is None:
+        print('flag')
+        template = "blog/create-view.html"
+
+    if slug is not None:
+        obj = get_object_or_404(PostModel, slug=slug)
+        print(f"if slug is not none ----->{obj}")
+        context["object"] = obj
+        template = "blog/detail-view.html"
+        if "edit" in request.get_full_path():
+            template = "blog/update-view.html"
+        elif "delete" in request.get_full_path():
+            template = "blog/delete-view.html"
+            if request.method == "POST":
+                obj.delete()
+                messages.success(request, "Deleted blog post!")
+                return HttpResponseRedirect(f"/blog/")
+
+    form = PostModelForm(request.POST or None, instance=obj)
+    context["form"] = form
+    if form.is_valid():
+        obj2 = form.save(commit=False)
+        print(f"------>{obj}<------")
+        obj2.save()
+        # messages.success("request", success_message)
+        if obj is not None:
+            return HttpResponseRedirect(f"/blog/{obj.slug}")
+        context["form"] = PostModelForm()
+
+    return render(request, template, context)
